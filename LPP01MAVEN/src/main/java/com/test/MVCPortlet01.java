845f780ab37a1f02d16394dac035314defbb0046
EKuryshb <EKuryshb@HQW0629.eub.kz>
2017-05-03 10:14:41 +0600

    Mvc Portlet

---
+++ LPP01MAVEN/src/main/java/com/test/MVCPortlet01.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class MVCPortlet01
+ */
+public class MVCPortlet01 extends MVCPortlet {
+ 
+
+}
