281f57acc0ce463f8947c553392a9acb9592722f
yerkebulan <yerke1984@gmail.com>
2017-05-13 17:21:31 +0600

    1

--- LPP_REACTJS_01/src/main/webapp/js/main_by_babel.js
+++ LPP_REACTJS_01/src/main/webapp/js/main_by_babel.js
@@ -1,12 +1,43 @@
 "use strict"; // этот код будет работать по современному стандарту ES5
 
 var Calculator = React.createClass({
+	
+	getInitialState:function () {
+		return {
+			mymath:[
+				["C", "±", "%", "÷"],
+				[7, 8, 9, "×"],
+				[4, 5, 6, "-"],
+				[1, 2, 3, "+"],
+				[0, ",", "="]
+			]
+		}		
+	},
 
-	render: function () {
-        var info = "1234567890 C ± + - ÷ × % AC !";
+	render: function () {        
         return (
         		
-            <div id="calculator"></div>
+            <div id="calculator">
+            	<div id="result"></div>
+            	<div id="mathtable">
+            	
+            		{this.state.mymath.map(
+            				function(row){
+            					var items = row.map(
+            							function(operand){
+            								return(
+            										<div>{operand}</div>
+            								)
+            					})
+            					
+            					return(
+            							<div>{items}</div>
+            					)
+            				}            		            				
+            		)}
+            		
+            	</div>            
+            </div>
                         
         );
     }
