6e13af6d197227347964cd6232ec7c15bf971ca3
yerkebulan <yerke1984@gmail.com>
2017-05-13 19:47:10 +0600

    1

--- LPP_REACTJS_01/src/main/webapp/js/main_by_babel.js
+++ LPP_REACTJS_01/src/main/webapp/js/main_by_babel.js
@@ -1,42 +1,36 @@
 "use strict"; // этот код будет работать по современному стандарту ES5
 
 var Calculator = React.createClass({
-	
-	getInitialState:function () {
-		return {
-			mymath:[
-				["C", "±", "%", "÷"],
-				[7, 8, 9, "×"],
-				[4, 5, 6, "-"],
-				[1, 2, 3, "+"],
-				[0, ",", "="]
-			]
-		}		
-	},
 
-	render: function () {        
+	render: function () {
+		
+		var mymath = [
+			["C", "±", "%", "÷"],
+			[7, 8, 9, "×"],
+			[4, 5, 6, "-"],
+			[1, 2, 3, "+"],
+			[0, "", ",", "="]
+		];
+	
         return (
         		
             <div id="calculator">
             	<div id="result"></div>
-            	<div id="mathtable">
-            	
-            		{this.state.mymath.map(
-            				function(row){
-            					var items = row.map(
-            							function(operand){
-            								return(
-            										<div>{operand}</div>
-            								)
-            					})
-            					
+            	<div id="mathtable"><table className="mtable">            	
+            		{
+            			mymath.map(function(oneRow, i){
+            				var items = oneRow.map(function(oneItem, j){            					
             					return(
-            							<div>{items}</div>
-            					)
-            				}            		            				
-            		)}
-            		
-            	</div>            
+            							<td className={
+            									oneItem === 0 ? 'myitem nolitem'
+            											: j == 3 ? 'myitem myitem_orange'
+            													: 'myitem'}>{oneItem}</td>
+            					);            					
+            				});
+            				return (<tr className="myrow">{items}</tr>);            			
+            			})            		
+            		}            		
+            	</table></div>            
             </div>
                         
         );
