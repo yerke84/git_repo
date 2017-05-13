f4f988b0c90b916e87b46980314dba4db4c931bb
yerkebulan <yerke1984@gmail.com>
2017-05-13 13:45:10 +0600

    babel

--- LPP_REACTJS_01/src/main/webapp/js/main_by_babel.js
+++ LPP_REACTJS_01/src/main/webapp/js/main_by_babel.js
@@ -1,6 +1,9 @@
+
+
+
 var Lander = React.createClass({
     render: function () {
-        var info = "1234567890+-/*=c";
+        var info = Math.random() + "_*_1234567890+-/*=c";
         return(
             <div>
                 <div className="info">{info}</div>
