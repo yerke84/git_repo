ff3d09c7cf341abdcdda0e948446e635823cf7fa
yerkebulan <yerke1984@gmail.com>
2017-05-13 11:46:58 +0600

    4

--- LPP_REACTJS_01/src/main/webapp/js/main2.js
+++ LPP_REACTJS_01/src/main/webapp/js/main2.js
@@ -1,6 +1,6 @@
 var Lander = React.createClass({
     render: function () {
-        var info = "Lorem ipsum dolor sit amet... ";
+        var info = "Lorem ipsum dolor sit amet... 111";
         return(
             <div>
                 <div className="info">{info}</div>
