f782b29cf01798395e78d4cf7003421137d4dad0
EKuryshb <EKuryshb@hqw0629.eub.kz>
2017-05-15 17:29:21 +0600

    Invoke Rest WS by React JS

--- LPP_REACTJS_01/src/main/webapp/js/invokeRest.js
+++ LPP_REACTJS_01/src/main/webapp/js/invokeRest.js
@@ -5,7 +5,8 @@ var InvokeRestClass = React.createClass({
 	getInitialState: function() {
 		return {
 			enteredValue: 'United States',
-			result: null
+			result: null,
+			list: []
 		}		
 	},
 	
@@ -14,29 +15,42 @@ var InvokeRestClass = React.createClass({
 		var ss = this.state.enteredValue.trim();		
 		if(!ss) {
 			this.setState({
-				result: "Please, enter country name."  
+				result: "Please, enter country name.",
+				list: []
 			})
 		} else {
-			var webServiceURL = 'http://www.webservicex.net/globalweather.asmx/GetCitiesByCountry?CountryName=' + ss;
-			
-			
 			/*
+			 * axios({ method: "get", url: webServiceURL, headers: {
+			 * 'crossDomain': true, dataType : 'jsonp' } })
+			 * .then(function(response){ console.log(response.data);
+			 * console.log(response.status); console.log(response);
+			 * }).catch(function(error){ console.log(error.data);
+			 * console.log(error.status); console.log(error); });
+			 */
+						
+			var webServiceURL = 'http://services.groupkt.com/country/search?text=' + ss;
+									
 			$.ajax({
-				url : webServiceURL,
-				type : "GET",
-				dataType : "jsonp",
-				crossDomain: true,				
-				success : function(resp){					
-					this.setState({result: String(resp)});					
+				url : webServiceURL,				
+				success : function(resp){
+					var ss = String(resp.RestResponse.messages[1]);
+					this.setState({result : ss})
+										
+					var arr = resp.RestResponse.result;
+					var mylist = [];
+					
+					for(var i = 0; i < arr.length; i++){
+						mylist.push(arr[i].name + "(" + arr[i].alpha2_code + "/" + arr[i].alpha3_code +")");						
+					}					
+					this.setState({list : mylist})
+															
 				}.bind(this),				
 				error : function(error){
-					console.log(error.status);					
-					console.log(error.statusText);
-					console.log(error.string);
-					//this.setState({result: jqXHR.status + ": " + jqXHR.statusText});					
+					this.setState({result : error.status + ": " + error.statusText})
 				}.bind(this)
 			});
-			*/
+			
+			
 		}
 		
 	},
@@ -57,6 +71,13 @@ var InvokeRestClass = React.createClass({
 		  		<button className="searchBtn" onClick={this.getCities.bind()}>Get Cities List</button>
 		  	</p>
 			<div className="listFnt">{this.state.result}</div>
+			<div className="listFnt"><ul>
+			{
+				this.state.list.map(function(item){
+					return <li>{item}</li>					
+				})					
+			}
+			</ul></div>
 		  </div>
 		)
 	}
