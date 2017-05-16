90cacfe68e7e00c0f37d1e3016aba9e2e5331219
EKuryshb <EKuryshb@HQW0629.eub.kz>
2017-05-16 14:04:33 +0600

    1

--- DWP_FOR_LR/src/test/TestClass.java
+++ DWP_FOR_LR/src/test/TestClass.java
@@ -4,20 +4,16 @@ import java.util.Random;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 @Path("/test_rest_class")
 public class TestClass {
 
 	@GET
 	@Path("/get_random")
-	@Produces(MediaType.APPLICATION_XML)
-	// @Produces(MediaType.TEXT_PLAIN + ";charset=utf-8")
 	public String getRandom() {
 		Random r = new Random();
-		int i = r.nextInt(1000000);
-		return "<root><v>" + String.valueOf(i) + "</v></root>";
+		int i = r.nextInt(1000);
+		return String.valueOf(i);
 	}
 
 }
