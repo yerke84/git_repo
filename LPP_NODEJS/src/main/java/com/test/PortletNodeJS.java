bb31d15f535027701c1f2b8b0d68aef018cdaecc
yerkebulan <yerke1984@gmail.com>
2017-06-10 10:05:17 +0600

    liferay & node js (1)

---
+++ LPP_NODEJS/src/main/java/com/test/PortletNodeJS.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PortletNodeJS
+ */
+public class PortletNodeJS extends MVCPortlet {
+
+}
